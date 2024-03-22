41ca2886020414ff8e7a507d4f1056be7308ba45
brunogperez <129124577+brunogperez@users.noreply.github.com>
2024-03-22 15:22:24 -0300

    Dccumentación API REST

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ import sessionRouter from './routes/session.router.js'
 import cors from 'cors'
 import __dirname from './utils.js'
 import cookieParser from 'cookie-parser'
-import swaggerJSDoc from 'swagger-jsdoc'
-import SwaggerUiExpress from 'swagger-ui-express'
+// import swaggerJSDoc from 'swagger-jsdoc'
+// import SwaggerUiExpress from 'swagger-ui-express'
 import { addLogger, logger } from './utils/logger.js'
 import { PORT } from './config/config.js'
 
@@ -23,26 +23,26 @@ import { PORT } from './config/config.js'
 
 const app = express()
 
-app.use(addLogger)
+// app.use(addLogger)
 
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.1',
-    info: {
-      title: 'Coderhouse Ecommerce Documentacion',
-      description: 'Este es un proyecto educativo que sirve como api para un simple ecommerce.'
-    }
-  },
-  apis: [`${__dirname}/docs/**/*.yaml`]
-}
+//  const swaggerOptions = {
+//    definition: {
+//      openapi: '3.0.1',
+//      info: {
+//        title: 'Coderhouse Ecommerce Documentacion',
+//        description: 'Este es un proyecto educativo que sirve como api para un simple ecommerce.'
+//      }
+//    },
+//    apis: [`${__dirname}/docs/**/*.yaml`]
+//  }
 
-const specs = swaggerJSDoc(swaggerOptions)
-app.use('/apidocs', SwaggerUiExpress.serve, SwaggerUiExpress.setup(specs))
+//  const specs = swaggerJSDoc(swaggerOptions)
+//  app.use('/apidocs', SwaggerUiExpress.serve, SwaggerUiExpress.setup(specs))
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/static', express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.use(cors())
 app.use(cookieParser())
