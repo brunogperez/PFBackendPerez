6f29689d9bb69e2f8f91bf5dbef45f6714209232
brunogperez <129124577+brunogperez@users.noreply.github.com>
2024-03-22 15:42:27 -0300

    Dccumentación API REST - cambios en enviroment

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,10 @@
-import {v2 as cloudinary} from 'cloudinary';
-          
-cloudinary.config({ 
-  cloud_name: 'divujqlv8', 
-  api_key: '754779667166728', 
-  api_secret: 'TNGz4rDjJ58BaKMNbUCkJ9yY66A' 
+import { v2 as cloudinary } from 'cloudinary';
+import { CLOUD_API_KEY, CLOUD_API_SECRET, CLOUD_NAME } from './config';
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_API_KEY,
+  api_secret: CLOUD_API_SECRET
 });
 
 export { cloudinary }
